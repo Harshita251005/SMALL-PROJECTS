d57fdefe7ee81a0d9acc1d6b99f33df1ed77d3cb
Harshita <harshita0601.be23@chitkara.edu.in>
2025-02-11 22:05:10 +0530

    done

---
+++ PORTFOLIO/portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:[" Java Programmer ","Aspiring Software Engineer "," Problem Solver "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
